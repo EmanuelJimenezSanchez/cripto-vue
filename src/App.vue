3e810c6cefcf7f361c153630c16f0b4695951893
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2024-01-28 23:02:53 -0600

    Spinner para el waiting time de la API

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import { ref, reactive, onMounted, computed } from 'vue'
   import Alerta from './components/Alerta.vue'
+  import Spinner from './components/Spinner.vue'
 
   const monedas = ref([
       { codigo: 'USD', texto: 'Dolar de Estados Unidos'},
@@ -16,6 +17,7 @@
     criptomoneda: ''
   })
   const cotizacion = ref({})
+  const cargando = ref(false)
 
   onMounted(() => {
     const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD'
@@ -36,13 +38,23 @@
   }
 
   const obtenerCotizacion = async () => {
-    const { moneda, criptomoneda } = cotizar
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
+    cargando.value = true
+    cotizacion.value = {}
 
-    const respuesta = await fetch(url)
-    const data = await respuesta.json()
+    try {
+      const { moneda, criptomoneda } = cotizar
+      const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
 
-    cotizacion.value = data.DISPLAY[criptomoneda][moneda]
+      const respuesta = await fetch(url)
+      const data = await respuesta.json()
+
+      cotizacion.value = data.DISPLAY[criptomoneda][moneda]
+    } catch (error) {
+      console.log(error)
+    } finally {
+      cargando.value = false
+    }
+
   }
 
 const mostrarResultado = computed(() => {
@@ -90,6 +102,10 @@ const mostrarResultado = computed(() => {
 
         <input type="submit" value="Cotizar">
       </form>
+
+      <Spinner 
+        v-if="cargando"
+      />
 
       <div 
         class="contenedor-resultado"
