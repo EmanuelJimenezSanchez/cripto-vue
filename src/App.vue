242eb5ed07060bd8a5b17a4c5c8f5d32ddf7ae4e
Emanuel Jimenez <emanuel.jimenez9123@alumnos.udg.mx>
2024-01-28 22:49:07 -0600

    Add reactive state and error handling for cryptocurrency quotes

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-  import { ref, reactive, onMounted } from 'vue'
+  import { ref, reactive, onMounted, computed } from 'vue'
   import Alerta from './components/Alerta.vue'
 
   const monedas = ref([
@@ -44,6 +44,10 @@
 
     cotizacion.value = data.DISPLAY[criptomoneda][moneda]
   }
+
+const mostrarResultado = computed(() => {
+    return Object.values(cotizacion.value).length > 0
+  })
 </script>
 
 <template>
@@ -86,6 +90,27 @@
 
         <input type="submit" value="Cotizar">
       </form>
+
+      <div 
+        class="contenedor-resultado"
+        v-if="mostrarResultado"
+      >
+        <h2>Cotizacion</h2>
+
+        <div class="resultado">
+          <img 
+            :src="'https://cryptocompare.com/' + cotizacion.IMAGEURL" 
+            alt="imagen cript"
+          >
+
+          <div>
+            <p>El precio es de: <span>{{ cotizacion.PRICE }}</span></p>
+            <p>Precio mas alto del dia: <span>{{ cotizacion.HIGHDAY }}</span></p>
+            <p>Variacion ultimas 24 horas: <span>{{ cotizacion.CHANGEPCT24HOUR }}%</span></p>
+            <p>Ultima actualizacion: <span>{{ cotizacion.LASTUPDATE }}</span></p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
